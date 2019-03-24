e46485f72a5c41f34334675df19bbac954386e78
Daniel Ugrin <ugrindaniel@gmail.com>
2019-03-24 21:21:34 +0100

    Blockly block name refactor CommandCreater added

---
+++ Senzor/Components/Blockly/CommandCreator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senzor.Components.Blockly
+{
+    public class CommandCreator
+    {
+        private List<Block> blocks;
+        public void setBlocks(List<Block> blocks) => this.blocks = blocks;
+        public void generateCommands()
+        {
+            
+        }
+
+    }
+}
